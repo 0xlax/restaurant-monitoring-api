800721087dbf59aabc6bcdaadb3f7dce159d4d5e
0xlax <harshu8310@gmail.com>
2023-06-03 01:40:20 +0530

    Base route example

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-import flask
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify(message='Hello, world!')
+
+if __name__ == '__main__':
+    app.run()
